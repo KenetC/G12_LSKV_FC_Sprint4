d1da44c47f5b3083e0e786973a4a0b1b1bad2899
KenetC <kenetchape@gmail.com>
2021-12-20 11:28:47 -0300

    Heroku

--- src/app.js
+++ src/app.js
@@ -28,8 +28,6 @@ app.use( (req,res,next)=>{
     res.status(404).render('not-found');
 });
 
-
-
 app.listen (process.env.PORT || port, () =>{
     console.log(`Servidor funcionando en el puerto ${port}` )
 })
