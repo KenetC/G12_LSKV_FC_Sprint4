79f860f75977fcf228d448b077655819e8523825
vanina <vanina.vilte@hotmail.com>
2021-12-23 01:46:06 -0300

    Vanina - locals y contact

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -2,7 +2,13 @@ const homeController = {
     
    index: (req,res) =>{
         return res.render("home1/home");
-        }
+        },
+   locals: (req,res) =>{
+        return res.render("locals");
+        },
+   contact: (req,res) =>{
+        return res.render("contact");
+        },
 }
 
 module.exports = homeController; 
--- src/routers/homeRoutes.js
+++ src/routers/homeRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', controllers.index);
-
+router.get('/locals', controllers.locals);
+router.get('/contact', controllers.contact);
 
 module.exports = router;
